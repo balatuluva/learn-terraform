c862c99429bcdfea6bc1a7810477f502823e5fb4
balatuluva <bala.tulva@gmail.com>
2023-04-28 18:29:54 +0100

    output declaration syntax

--- 01-Variables/Variables.tf
+++ 01-Variables/Variables.tf
@@ -1,4 +1,17 @@
-variable "sample_script" {
+# In shell scripting we declare them like sample_string="Hello World"
+variable "sample_string" {
   default = "Hello World"
 }
-# in shell scripting we declare them like sample_script="Hello World"
+
+
+# To print in shell we use, echo $sample_string
+output "sample_string" {
+  value = var.sample_string
+}
+
+
+## In above case we are printing just variable, If variable is a combination of some strings then syntax is:
+output "sample_string1" {
+  value = "value of sample_string = ${var.sample_string}
+}
+
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # learn-terraform
 
 1. Variables
-2. Data Source
-3. Resource
-4. Module
-5. Locals
-6. Provider
-7. State File
-8. Loops
-9. Conditions
+2. functions
+3. Data Source
+4. Resource
+5. Module
+6. Locals
+7. Provider
+8. State File
+9. Loops
+10. Conditions
+11. Output
