aab23e8b73f31819e0294f618dbda35ac47ad45e
balatuluva <bala.tulva@gmail.com>
2023-05-01 10:39:40 +0100

    terraform tfvars

--- 01-Variables/Variables.tf
+++ 01-Variables/Variables.tf
@@ -29,3 +29,4 @@ variable "sample_dict" {
 
 variable "env" {}
 variable "auto_num1" {}
+variable "sample1" {}
--- 01-Variables/output.tf
+++ 01-Variables/output.tf
@@ -33,3 +33,9 @@ output "auto_num1" {
   value = var.auto_num1
 }
 
+
+
+output "sample1" {
+  value = var.sample1
+}
+
