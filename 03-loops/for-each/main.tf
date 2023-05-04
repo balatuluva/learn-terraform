00ce62179f08707c99dc70033be97765d2f7f5b0
balatuluva <bala.tulva@gmail.com>
2023-05-04 07:03:40 +0100

    for each loops with multiple maps

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -4,12 +4,26 @@ resource "null_resource" "fruits" {
     command = "echo Fruit Name - ${each.key} - ${each.value}"
   }
 }
-
-
 variable "fruits" {
   default = {
     apple = 10
     banana = 20
     orange = 30
+  }
+}
+
+
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key} - ${each.value["count"]}"
+  }
+}
+variable "fruits1" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
   }
 }
