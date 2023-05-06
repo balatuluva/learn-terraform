bacad71a6796a483640d97547a28e728bfa830d5
balatuluva <bala.tulva@gmail.com>
2023-05-06 12:49:13 +0100

    functions list and lookup

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -2,7 +2,25 @@
 variable "sample" {
   default = "abcxyz"
 }
-
 output "sample" {
   value = upper(var.sample)
+}
+
+# Print first value in list using functions
+variable "sample1" {
+  default = ["abc", "xyz"]
+}
+output "sample1" {
+  value = element(var.sample1, 0)
+}
+
+# Lookup function
+variable "sample2" {
+  default = {
+    abc = 100
+    xyz = 200
+  }
+}
+output "sample2" {
+  value = lookup(var.sample1, "a1", "dummy")
 }
